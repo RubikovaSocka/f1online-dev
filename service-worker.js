31066a04ff0e3f9dce7d829a3fd9427429983fa6
RubikovaSocka <mincorbr@gmail.com>
2020-04-01 01:02:44 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/f1online-dev/precache-manifest.157282e2e976773db17ba832580909bf.js"
+  "/f1online-dev/precache-manifest.3c84419fe966ae63399f4b574b0a42ee.js"
 );
 
 self.addEventListener('message', (event) => {
