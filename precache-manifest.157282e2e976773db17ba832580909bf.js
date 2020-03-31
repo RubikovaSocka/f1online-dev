aec7c2b3699f1eed7c0e917d10db21dd2deba19f
RubikovaSocka <mincorbr@gmail.com>
2020-04-01 00:32:16 +0200

    Updates

---
+++ precache-manifest.157282e2e976773db17ba832580909bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47b75b1ffcaebc4390c4c8ed5ed94dae",
+    "url": "/f1online-dev/index.html"
+  },
+  {
+    "revision": "2a76bccf04db483002df",
+    "url": "/f1online-dev/static/css/main.ce0f4677.chunk.css"
+  },
+  {
+    "revision": "199b5cbfb1bb13e59075",
+    "url": "/f1online-dev/static/js/2.97701bb5.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/f1online-dev/static/js/2.97701bb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a76bccf04db483002df",
+    "url": "/f1online-dev/static/js/main.94c48481.chunk.js"
+  },
+  {
+    "revision": "51c9e2eaad0a8f4e9d96",
+    "url": "/f1online-dev/static/js/runtime-main.857708bd.js"
+  }
+]);
